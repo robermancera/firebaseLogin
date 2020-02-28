c9fd404d0905b329db14ae805c2f1befe777b7ac
robermancera <48019891+robermancera@users.noreply.github.com>
2020-02-28 10:15:15 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC6XlTo6JAxVWmcfoA20lb60EqOHuGDmc8",
+    authDomain: "controlac-e93d2.firebaseapp.com",
+    databaseURL: "https://controlac-e93d2.firebaseio.com",
+    projectId: "controlac-e93d2",
+    storageBucket: "controlac-e93d2.appspot.com",
+    messagingSenderId: "sender-id",
+    appID: "app-id",
+  };
+
+firebase.initializeApp(firebaseConfig);
+
