b01b2f15decd1e55a7f3e7489b9dd239855d949d
robermancera <48019891+robermancera@users.noreply.github.com>
2020-03-01 11:35:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,53 @@ var firebaseConfig = {
     projectId: "controlac-e93d2",
     storageBucket: "controlac-e93d2.appspot.com",
     messagingSenderId: "sender-id",
-    appID: "app-id",
-  };
+    appID: "1:670813732118:web:379efa7ca63c627e7a2437"
+  }
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
+
+  
+  
+ 
+const form = document.forms['loginForm']
+form.addEventListener('submit', function handleFormSubmit(event){
+  const email = form['email'].value
+  const password = form['password'].value
+  const isLoginOrSignup = form['isLoginOrSignup'].value
+
+  if (isLoginOrSignup === 'isLogin') {
+    return loginUser(email, password)
+  }
+
+  return createUser(email, password)
+})
+
+function createUser(email, password) {
+    console.log('Creando el usuario con email ' + email);
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function (user) {
+        console.log('¡Creamos al usuario!');
+    })
+    .catch(function (error) {
+        console.error(error)
+    });
+}
+
+function loginUser(email, password) {
+    console.log('Loging user ' + email);
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function (user) {
+        console.log('Credenciales correctas, ¡bienvenido!');
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
+function signoutUser() {
+    firebase.auth().signOut();
+}
+
 
